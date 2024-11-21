005072a7a0c4079f48ed1a956a6a5a97fcba03c0
guesswhojustme <imnayeonsaranghaee@gmail.com>
2024-11-21 21:46:53 +0800

    Solve the problem

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
 
+const getTheTitles = function(array) {
+    const titles = array.map(arrayTitles => arrayTitles.title);
+
+    return titles;
 };
+
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
